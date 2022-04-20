19786cee52bb14f0032c50ae0701e536aaf90506
Adam Bentley <61187053+Adambentleyio@users.noreply.github.com>
2022-04-20 12:36:14 +0100

    home page complete

---
+++ index.js
@@ -0,0 +1,18 @@
+// when someone clicks the hamburger button
+// if the nav is clsoed, open it
+// if the nav is open, close it
+
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = nav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", "true");
+    navToggle.setAttribute("aria-expanded", "true");
+  } else {
+    nav.setAttribute("data-visible", "false");
+    navToggle.setAttribute("aria-expanded", "false");
+  }
+});
